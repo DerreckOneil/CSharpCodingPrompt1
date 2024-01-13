c45fdcbd93485d02c5442c16ea7be2e4e9ef07af
Derreck Oneil <derreckoneil@gmail.com>
2024-01-13 15:23:59 -0500

    Problem 1 Done.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Program
+{
+
+    /*
+    Create a function that takes a string. The function must return the given string in reverse order. 
+    The Reverse method from the Array class is not allowed to be used. Comment: if reverse was available this would've been over in two seconds. 
+    */
+
+
+    static void Main()
+    {
+        string randomString = "foobar";
+
+        Console.WriteLine("Soluton = " + PassInAString(randomString));
+    }
+
+    static string PassInAString(string str)
+    {
+        char[] chars = str.ToCharArray();
+        string solution = "";
+
+        for(int i = chars.Length-1; i >= 0; i-- )
+        {
+            solution += chars[i];
+        }
+
+        return solution;
+    }
+}
